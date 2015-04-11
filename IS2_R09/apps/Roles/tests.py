cd69d9eeac2fc7609297dd42083ab930040987c9
Samuel Ruiz <ssruiz6@gmail.com>
2015-04-11 01:11:58 -0400

    Pruebas Unitarias - Proyecto,Roles
    Se agregaron algunas pruebas unitarias para Proyecto y
    Roles.
    Pequeñas pero suficientes para comprobar el funcionamiento.

--- IS2_R09/apps/Roles/tests.py
+++ IS2_R09/apps/Roles/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+import unittest
+from django.contrib.auth.models import User,Group
+ 
 # Create your tests here.
+class test_content(TestCase):
+    def setUp(self):
+        # se inicializa un Group de django
+        
+        g1= Group.objects.create(name="Rol1")
+        
+        
+    def test_rol_nombre(self):
+        grp = Group.objects.get(name = "Rol1")
+        print "Test 1. Imprimiendo nombre del Rol --> %s" %(grp.name)
+        

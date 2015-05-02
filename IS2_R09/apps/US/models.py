c915737814edc2278de417f0d8e7b435cf6204de
meliam <meli452@gmail.com>
2015-05-01 21:52:08 -0400

    Modificacion de User Story para habilitar Adjunto

--- IS2_R09/apps/US/models.py
+++ IS2_R09/apps/US/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from IS2_R09.apps.Flujo.models import flujo
 from IS2_R09.apps.Proyecto.models import proyecto
+from django.db.models.fields.related import ManyToManyField
 # Create your models here.
 class us(models.Model):
     PRIORIDADES = (
@@ -19,5 +20,6 @@ class us(models.Model):
     prioridad=models.CharField(max_length=1, choices=PRIORIDADES)
     flujo_asignado=models.OneToOneField(flujo,null=True,blank=True)
     proyecto_asociado=models.OneToOneField(proyecto,null=True,blank=True)
+    id_adjunto = ManyToManyField(adjunto,null=True,blank=True)
     def __str__(self):
         return self.nombre
